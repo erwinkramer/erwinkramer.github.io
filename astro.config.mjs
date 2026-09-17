5de06f182d3c0f209631ea31d80e61cf8f3efb71
Erwin Kramer <erwinkramer@hotmail.com>
2026-09-17 21:08:25 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,10 +42,6 @@ export default defineConfig({
       customCss: ['./src/styles/space-theme.css'],
       head: [
         {
-          tag: 'style',
-          content: ':root{color-scheme:dark;background-color:#090808}',
-        },
-        {
           tag: 'link',
           attrs: {
             rel: 'preload',
